961943ce76fc63c48d97d8eb63f713dc65dfe2ea
zahraaAhmed <zahraa.ahmed.ham@gmail.com>
2023-02-26 22:24:13 +0200

    profile page

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".subscribe-btn").click(function() {
+        $(".subscribe-block").addClass("d-none");
+        $(".notification-block").removeClass("d-none");
+    })
+})
