fe05a6626211b474144e3ec222081a9498020ff0
zahraaAhmed <zahraa.ahmed.ham@gmail.com>
2023-02-28 17:41:59 +0200

    login&logout&register&menu

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
     $(".subscribe-btn").click(function() {
         $(".subscribe-block").addClass("d-none");
         $(".notification-block").removeClass("d-none");
+    });
+    $(".navbar-toggler").click(function() {
+        $(".small-menus").toggleClass("show");
+        $("body").toggleClass("hide-scroll");
     })
 })
